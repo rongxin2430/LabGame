3f2e950517379b3ecc6ff3f038b8d13b558e770a
rongxin2430 <rongxin233472430@gmail.com>
2023-05-05 10:47:55 -0400

    Editing on the health of balloon

--- Assets/Blocking.cs
+++ Assets/Blocking.cs
@@ -6,15 +6,16 @@ using UnityEngine.SocialPlatforms.Impl;
 
 public class Blocking : MonoBehaviour
 {
-    public int health = 30;
+    [SerializeField] public int health = 30;
     Rigidbody2D body;
-    // Start is called before the first frame update
+
     public void hit(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
             Break();
+            
         }
     }
     void Break()
